9bf30fa0ab70752460dd48f98279993170f6321e
Nishanth Koganti <buntyke@gmail.com>
2015-08-16 13:09:52 +0900

    add zeromq interface to marker recorder

--- nodes/marker_recorder.cpp
+++ nodes/marker_recorder.cpp
@@ -2,9 +2,11 @@
 // Author: Nishanth Koganti
 // Date: 2015/8/15
 
+#include <zmq.hpp>
 #include <fstream>
 #include <iostream>
 #include <ros/ros.h>
+#include <ros/spinner.h>
 #include <std_msgs/String.h>
 #include <ar_track_alvar_msgs/AlvarMarkers.h>
 
@@ -48,20 +50,90 @@ int main(int argc, char **argv)
   // subscribe to ros topic
   ros::Subscriber sub = n.subscribe("ar_pose_marker", 50, callback);
 
-  // create and initialize file stream
-  fOut.open(argv[1]);
+  // AsyncSpinner is used to synchronize multiple threads under the ros framework
+  // URL:http://wiki.ros.org/roscpp/Overview/Callbacks%20and%20Spinning#Multi-threaded_Spinning
+  ros::AsyncSpinner spinner;
 
-  // initialize frame and time variables
-  ros::Rate r(30);
-  begin = ros::Time::now();
+  // zeromq Initialization
+  zmq::context_t context(1);
+  zmq::socket_t kinectSocket(context, ZMQ_PAIR);
 
-  // starting ros subscriber
-  while (ros::ok())
-  {
-    ros::spinOnce();
-    r.sleep();
-  }
+  // create server
+  kinectSocket.bind("tcp://*:5555");
+  std::cout << "[ZMQ] Created kinect server" << std::endl;
 
-  fOut.close();
+  while (1)
+  	{
+  		// Get the file name
+  		zmq::message_t request;
+  		kinectSocket.recv(&request);
+  		Message = std::string(static_cast<char *>(request.data()), request.size());
+
+      if (Message == "StopServer")
+  		{
+  			std::cout << "[ZMQ] Received Stop Server" << std::endl;
+  			break;
+  		}
+
+      else if (Message == "NewTrial")
+  		{
+  			std::cout << "[ZMQ] Received NewTrial" << std::endl;
+
+  			// send ready reply
+  			zmq::message_t reply(5);
+  			std::memcpy((void *)reply.data(), "Ready", 5);
+  			kinectSocket.send(reply);
+  			std::cout << "[ZMQ] Sent Ready" << std::endl;
+
+  			// get kinect filename
+  			zmq::message_t recordName;
+  			kinectSocket.recv(&recordName);
+  			fileName = std::string(static_cast<char *>(recordName.data()), recordName.size());
+  			std::cout << "[ZMQ] Received filename" << std::endl;
+
+        // create and initialize file stream and write file header
+        fOut.open(argv[1]);
+        fOut << "Frame,Time,X,Y,Z" << std::endl;
+
+        zmq::message_t startRequest;
+  			kinectSocket.recv(&startRequest);
+  			Message = std::string(static_cast<char *>(startRequest.data()), startRequest.size());
+        if (Message == "StartRecording")
+        {
+          std::cout << "[ZMQ] Received StartRecording" << std::endl;
+
+          // initialize frame and time variables
+          begin = ros::Time::now();
+
+          // start recording
+          spinner.start();
+        }
+  			else
+        {
+          std::cout << "[ZMQ] Invalid message" << std::endl;
+          continue;
+        }
+
+        // Get the file name
+    		zmq::message_t stopRequest;
+    		kinectSocket.recv(&stopRequest);
+    		Message = std::string(static_cast<char *>(stopRequest.data()), stopRequest.size());
+        if (Message == "StopRecording")
+        {
+          std::cout << "[ZMQ] Received StopRecording" << std::endl;
+        }
+  			else
+        {
+          std::cout << "[ZMQ] Invalid message" << std::endl;
+        }
+
+        // stop the spinner
+        spinner.stop();
+        fOut.close();
+      }
+  	}
+
+  // clean shutdown
+  ros::shutdown();
   return 0;
 }
