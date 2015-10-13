68ce782e3407f02351ad63988c145ad9c0d8aa16
Nishanth Koganti <buntyke@gmail.com>
2015-10-13 21:59:09 +0900

    add transformation code

--- nodes/kinect_extract.cpp
+++ nodes/kinect_extract.cpp
@@ -30,11 +30,15 @@
 #include <pcl/features/vfh.h>
 #include <pcl/features/esf.h>
 #include <pcl_ros/point_cloud.h>
+#include <pcl/common/transforms.h>
 #include <pcl/features/normal_3d.h>
 #include <pcl/filters/voxel_grid.h>
 #include <pcl/visualization/cloud_viewer.h>
 #include <pcl/visualization/histogram_visualizer.h>
 #include <pcl/filters/statistical_outlier_removal.h>
+
+// OpenCV headers
+#include <opencv2/opencv.hpp>
 
 // help function
 void help(const std::string &path)
@@ -55,18 +59,23 @@ int main(int argc, char **argv)
   }
 
   // filename default
-  std::string fileName;
+  char c;
+  std::string fileName, calibName;
   char cloudName[200], esfName[200], vfhName[200], outputName[200];
 
   // printing help information
-  if(argc != 2)
+  if(argc != 3)
   {
     help(argv[0]);
     ros::shutdown();
     return 0;
   }
   else
+  {
     fileName = argv[1];
+    calibName = argv[2];
+  }
+
 
   // create fileNames for different output files
   sprintf(esfName, "%sESF", fileName.c_str());
@@ -89,6 +98,14 @@ int main(int argc, char **argv)
   bag.open(cloudName, rosbag::bagmode::Read);
   rosbag::View view(bag, rosbag::TopicQuery(topics));
 
+  ifstream calibDat(calibName, ifstream::in);
+  Eigen::Matrix4f transform = Eigen::Matrix4f::Identity();
+
+  for (int i = 0; i < 4; i++)
+    calibDat >> transform(i,0) >> c >> transform(i,1) >> c >> transform(i,2) >> c >> transform(i,3);
+  std::cout << transform.matrix() << std::endl;
+  std::cin >> c;
+
   // pcl point cloud
   pcl::visualization::CloudViewer viewer("Feature Extraction");
 
@@ -100,6 +117,7 @@ int main(int argc, char **argv)
   pcl::PointCloud<pcl::PointXYZ>::Ptr cloudVFH(new pcl::PointCloud<pcl::PointXYZ>());
   pcl::PointCloud<pcl::Normal>::Ptr cloudNormals(new pcl::PointCloud<pcl::Normal>());
   pcl::PointCloud<pcl::PointXYZ>::Ptr cloudCentered(new pcl::PointCloud<pcl::PointXYZ>());
+  pcl::PointCloud<pcl::PointXYZ>::Ptr cloudTransform(new pcl::PointCloud<pcl::PointXYZ>());
   pcl::search::KdTree<pcl::PointXYZ>::Ptr neTree(new pcl::search::KdTree<pcl::PointXYZ>());
   pcl::search::KdTree<pcl::PointXYZ>::Ptr vfhTree(new pcl::search::KdTree<pcl::PointXYZ>());
   pcl::PointCloud<pcl::VFHSignature308>::Ptr vfhs(new pcl::PointCloud<pcl::VFHSignature308>());
@@ -125,7 +143,6 @@ int main(int argc, char **argv)
   ros::Time::init();
 
   // ros iterator initialization
-  char c;
   int frame = 0;
   ros::Rate rate(30);
   ros::Duration tPass;
@@ -150,12 +167,11 @@ int main(int argc, char **argv)
     sor.setInputCloud(cloudVOG);
     sor.filter(*cloudSOR);
 
-    // Center point cloud
-    compute3DCentroid(*cloudSOR, centroid);
-    demeanPointCloud(*cloudSOR, centroid, *cloudCentered);
+    pcl::transformPointCloud(*cloudSOR, *cloudTransform, transform);
 
-    copyPointCloud(*cloudCentered, *cloudVFH);
-		copyPointCloud(*cloudCentered, *cloudESF);
+    // Center point cloud
+    pcl::compute3DCentroid(*cloudTransform, centroid);
+    pcl::demeanPointCloud(*cloudTransform, centroid, *cloudCentered);
 
     // Normal Estimation
     ne.setInputCloud(cloudCentered);
@@ -182,9 +198,9 @@ int main(int argc, char **argv)
     esfDat << endl;
     cout << "Write ESF" << endl;
 
-    outputDat << tPass.toSec() << "," << cloudSOR->size() << endl;
-    for (int i = 0; i < cloudSOR->size(); i++)
-      outputDat << cloudSOR->points[i].x << "," << cloudSOR->points[i].y << "," << cloudSOR->points[i].z << endl;
+    outputDat << tPass.toSec() << "," << cloudCentered->size() << endl;
+    for (int i = 0; i < cloudCentered->size(); i++)
+      outputDat << cloudCentered->points[i].x << "," << cloudCentered->points[i].y << "," << cloudCentered->points[i].z << endl;
     cout << "Write Filtered Point Cloud" << endl;
 
     viewer.showCloud(cloudCentered);
